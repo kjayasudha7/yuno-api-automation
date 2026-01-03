5f489f4de1c6bd62e78f0b9af581ac488e9f923a
kjayasudha7 <109783825+kjayasudha7@users.noreply.github.com>
2026-01-03 22:02:56 +0530

    Create test_data_loader.py

---
+++ utils/test_data_loader.py
@@ -0,0 +1,8 @@
+python
+import json
+from pathlib import Path
+
+def load_customers():
+    file_path = Path(__file__).parent.parent / "test_data" / "customers.json"
+    with open(file_path) as file:
+        return json.load(file)
