c1e0369d6d3849aeb777f7b3cca50270d4929f41
kjayasudha7 <109783825+kjayasudha7@users.noreply.github.com>
2026-01-05 21:39:38 +0530

    Update test_data_loader.py

--- utils/test_data_loader.py
+++ utils/test_data_loader.py
@@ -6,3 +6,8 @@ def load_customers():
     file_path = Path(__file__).parent.parent / "test_data" / "customers.json"
     with open(file_path) as file:
         return json.load(file)
+
+def load_cards():
+    file_path = Path(__file__).parent.parent / "test_data" / "cards.json"
+    with open(file_path) as file:
+        return json.load(file)
